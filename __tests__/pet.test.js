f1678d0b53505c4655d5ac63e84d5a04558b52f5
Laura Crawford <lhcrawford97@gmail.com>
2022-12-13 17:48:56 +0000

    test and function updated for naming pets

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -4,4 +4,8 @@ describe("constructor", () => {
   test("returns an object", () => {
     expect(new Pet("Fido")).toBeInstanceOf(Object);
   });
+  test("sets the name property", () => {
+    const pet = new Pet("Fido");
+    expect(pet.name).toEqual("Fido");
+  });
 });
