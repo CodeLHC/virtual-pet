ac87cb83a7cc07f5bc77b26fde3f85d1ac9fda27
Laura Crawford <lhcrawford97@gmail.com>
2022-12-15 18:14:08 +0000

    grow up test and prototype method

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -8,4 +8,16 @@ describe("constructor", () => {
     const pet = new Pet("Fido");
     expect(pet.name).toEqual("Fido");
   });
+  test("has an initial age of 0", () => {
+    const pet = new Pet("Fido");
+    expect(pet.age).toEqual(0);
+  });
+});
+
+describe("growUp", () => {
+  test("increments the age by one", () => {
+    const pet = new Pet("Fido");
+    pet.growUp();
+    expect(pet.age).toEqual(1);
+  });
 });
