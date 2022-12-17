581eaa45b49c71336da4b451f7216a3a819eaebb
Laura Crawford <lhcrawford97@gmail.com>
2022-12-17 13:35:17 +0000

    walk function with refactoring

--- src/pet.js
+++ src/pet.js
@@ -1,14 +1,27 @@
+const MAXIMUM_FITNESS = 10;
+const HUNGER_INCREASE = 5;
+const FITNESS_DECREASE = 3;
+const FITNESS_INCREASE = 4;
+
 function Pet(name) {
   this.name = name;
   this.age = 0;
   this.hunger = 0;
-  this.fitness = 10;
+  this.fitness = MAXIMUM_FITNESS;
 }
 
 Pet.prototype.growUp = function () {
   this.age += 1;
-  this.hunger += 5;
-  this.fitness -= 3;
+  this.hunger += HUNGER_INCREASE;
+  this.fitness -= FITNESS_DECREASE;
+};
+
+Pet.prototype.walk = function () {
+  if (this.fitness + FITNESS_INCREASE <= MAXIMUM_FITNESS) {
+    this.fitness += FITNESS_INCREASE;
+  } else {
+    this.fitness = MAXIMUM_FITNESS;
+  }
 };
 
 module.exports = Pet;
